48df80183b65f0cde0faab0a9fa4c91bc0aecf21
unknown <agumon43@naver.com>
2024-08-27 20:11:04 +0900

    [Feat] - 프로젝트 연결

---
+++ shopcooperation/shopcooperation/src/main/java/com/example/shopcooperation/ShopcooperationApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopcooperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopcooperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopcooperationApplication.class, args);
+	}
+
+}
---
+++ shopcooperation/shopcooperation/src/test/java/com/example/shopcooperation/ShopcooperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopcooperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopcooperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
